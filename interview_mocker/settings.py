bc4f51c4d13c47950cb6219821ea5453ffea06f9
Ganesh Patil <gp8007282@gmail.com>
2025-09-06 22:20:21 +0530

    Start the project

--- interview_mocker/settings.py
+++ interview_mocker/settings.py
@@ -136,6 +136,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 #edited
 AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+admin.site.site_header = "AI Interview Mocker Admin"
+admin.site.site_title = "AI Interview Mocker Portal"
+admin.site.index_title = "Welcome to AI Interview Mocker Dashboard"
