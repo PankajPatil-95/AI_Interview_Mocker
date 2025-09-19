bde420267a6f6cd0b0c3a31a8545beba9de615d5
Ganesh Patil <gp8007282@gmail.com>
2025-09-19 11:20:23 +0530

    add templates

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+#edited
 from rest_framework import generics, permissions
 from .models import User
 from .serializers import UserSerializer, RegisterSerializer
 
+def home(request):
+    return HttpResponse("WELCOME TO AI INTERVIEW MOCKER!!")
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
